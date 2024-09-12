80022e31306280385851980ec7c57d1e4aa773a0
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-09-12 16:21:28 +0100

    Create window

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,49 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 720;
+
+bool init();
+void close();
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+
+bool init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL could not initialise! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    screenSurface = SDL_GetWindowSurface(window);
+    return true;
+}
+
+void close() {
+    SDL_DestroyWindow(window);
+    window = NULL;
+    SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+    if (!init()) {
+        std::cout << "Failed to initialise" << std::endl;
+        return 1;
+    }
+
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+
+    close();
+    
+    return 0;
 }
