57a8ce467eea46ff74e00fc27511a7cfa55ce805
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-09-20 15:09:21 +0100

    Added inline keyword to settings constexpr

--- client/include/settings.h
+++ client/include/settings.h
@@ -1,17 +1,17 @@
 #pragma once
 
 namespace Constants {
-    constexpr float SCALE = 1.0f;
-    constexpr int MARGIN = static_cast<int>(45 * SCALE);
-    constexpr int SCREEN_WIDTH = static_cast<int>(960 * SCALE);
-    constexpr int SCREEN_HEIGHT = static_cast<int>(720 * SCALE);
-    constexpr int BALL_SIZE = static_cast<int>(10 * SCALE);
-    constexpr float BALL_SPEED = 0.6f * SCALE;
-    constexpr int PADDLE_WIDTH = static_cast<int>(10 * SCALE);
-    constexpr int PADDLE_HEIGHT = static_cast<int>(90 * SCALE);
-    constexpr float PADDLE_SPEED = 1.0f * SCALE;
-    constexpr int FONT_SIZE = static_cast<int>(60 * SCALE);
-    constexpr int NET_SPACING = static_cast<int>(20 * SCALE);
+    inline constexpr float SCALE = 1.0f;
+    inline constexpr int MARGIN = static_cast<int>(45 * SCALE);
+    inline constexpr int SCREEN_WIDTH = static_cast<int>(960 * SCALE);
+    inline constexpr int SCREEN_HEIGHT = static_cast<int>(720 * SCALE);
+    inline constexpr int BALL_SIZE = static_cast<int>(10 * SCALE);
+    inline constexpr float BALL_SPEED = 0.6f * SCALE;
+    inline constexpr int PADDLE_WIDTH = static_cast<int>(10 * SCALE);
+    inline constexpr int PADDLE_HEIGHT = static_cast<int>(90 * SCALE);
+    inline constexpr float PADDLE_SPEED = 1.0f * SCALE;
+    inline constexpr int FONT_SIZE = static_cast<int>(60 * SCALE);
+    inline constexpr int NET_SPACING = static_cast<int>(20 * SCALE);
 }
 
 enum class Direction {
